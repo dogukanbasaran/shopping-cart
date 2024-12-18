9223fa769d8f39dc4fa595b2b0b19a6b4f12c331
dogukanbasaran <dogukanbasarann@hotmail.com>
2024-12-18 23:06:05 +0300

    general UI is completed

---
+++ index.js
@@ -0,0 +1,105 @@
+import data from "./products.js";
+
+const productList = document.querySelector("#product-list");
+
+
+data.products.forEach((product) => {
+
+    const products = document.createElement("div");
+    products.classList.add("product");
+
+    // Supplier Box
+
+    const supplierBox = document.createElement("div");
+    supplierBox.classList.add("supplier-box");
+
+    const supplierTitle = document.createElement("span");
+    supplierTitle.classList.add("supplier-title");
+    supplierTitle.textContent = `Supplier: ${product.supplier}`;
+
+    const shippingStatus = document.createElement("span");
+    shippingStatus.classList.add("shipping-status");
+    shippingStatus.textContent = `${product.shippingStatus ? "shipping free" : "no shipping free"}`;
+    
+    supplierBox.appendChild(supplierTitle);
+    supplierBox.appendChild(shippingStatus);
+
+    // Product Details
+
+    const productDetails = document.createElement("div");
+    productDetails.classList.add("product-details");
+
+    const productImgContainer = document.createElement("div");
+    productImgContainer.classList.add("product-img");
+    const productImg = document.createElement("img");
+    productImg.src = `${product.productImage}`;
+    productImg.height = "150";
+    productImg.width = "150";
+    productImgContainer.appendChild(productImg);
+
+    const productTextContainer = document.createElement("div");
+    productTextContainer.classList.add("product-texts");
+
+    const productName = document.createElement("span");
+    productName.classList.add("product-name");
+    productName.textContent = `${product.productName}`;
+    productTextContainer.appendChild(productName);
+
+    const productDescription = document.createElement("div");
+    productDescription.classList.add("product-description");
+    productDescription.textContent = `${product.productDescription}`;
+    productTextContainer.appendChild(productDescription);
+
+    const hirePurchaseStatus = document.createElement("span");
+    hirePurchaseStatus.classList.add("hire-purchase-status");
+    hirePurchaseStatus.textContent = `${product.hirePurchaseStatus ? "Accepting installment payment" : "No installment payment"}`
+    productTextContainer.appendChild(hirePurchaseStatus);
+
+    productDetails.appendChild(productImgContainer);
+    productDetails.appendChild(productTextContainer);
+
+    // Product Operations
+
+    const productOperations = document.createElement("div");
+    productOperations.classList.add("product-operations");
+
+    const productPurchasing = document.createElement("div");
+    productPurchasing.classList.add("product-purchasing");
+    const amountDecrementBtn = document.createElement("button");
+    amountDecrementBtn.innerHTML = `<i class="fa-solid fa-minus"></i>`;
+    const amountInput = document.createElement("input");
+    amountInput.classList.add("product-amount");
+    amountInput.value = 1;
+    const amountIncrementBtn = document.createElement("button");
+    amountIncrementBtn.innerHTML = `<i class="fa-solid fa-plus"></i>`;
+
+    productPurchasing.appendChild(amountDecrementBtn);
+    productPurchasing.appendChild(amountInput);
+    productPurchasing.appendChild(amountIncrementBtn);
+
+    const productPrice = document.createElement("span");
+    productPrice.classList.add("product-price");
+    productPrice.textContent = `${product.productPrice} TL`;
+
+    const productRemoveBtn = document.createElement("button");
+    productRemoveBtn.innerHTML = `<i class="fa-solid fa-trash"></i>`;
+
+
+    productOperations.appendChild(productPurchasing);
+    productOperations.appendChild(productPrice);
+    productOperations.appendChild(productRemoveBtn);
+
+
+    // General Appending
+
+    products.appendChild(supplierBox);
+    products.appendChild(productDetails);
+    products.appendChild(productOperations);
+
+    productList.appendChild(products);
+
+
+    // Functions
+
+
+});
---
+++ products.js
@@ -0,0 +1,46 @@
+const data = {
+   products: [
+        {
+            supplier : "Hepsi Burada",
+            shippingStatus : true,
+            productImage : "/assets/img/lenovo-laptop.jpg",
+            productName : "Lenovo Laptop",
+            productDescription : "Intel Core i5 12450HX 16GB 512GB SSD Arc A530M Freedos 15.6",
+            productPrice : 25000,
+            hirePurchaseStatus : true,
+        },
+    
+        {
+            supplier : "Vatan Bilgisayar",
+            shippingStatus : false,
+            productImage : "/assets/img/lenovo-laptop.jpg",
+            productName : "Lenovo Laptop2",
+            productDescription : "Intel Core i5 12450HX 16GB 512GB SSD Arc A530M Freedos 15.6",
+            productPrice : 30000,
+            hirePurchaseStatus : true,
+        },
+    
+        {
+            supplier : "Ekmek Bilişim",
+            shippingStatus : true,
+            productImage : "/assets/img/lenovo-laptop.jpg",
+            productName : "Lenovo Laptop3",
+            productDescription : "Intel Core i5 12450HX 16GB 512GB SSD Arc A530M Freedos 15.6",
+            productPrice : 45000,
+            hirePurchaseStatus : false,
+        },
+
+        {
+            supplier: "Ekmek Bilişim",
+            shippingStatus: false,
+            productImage: "/assets/img/ourphoto.jpg",
+            productName: "Ekmek and Leo Skylared",
+            productDescription: "This is our photo",
+            productPrice: 9999999999999,
+            hirePurchaseStatus: true
+        }
+    ]
+    
+}
+
+export default data;
